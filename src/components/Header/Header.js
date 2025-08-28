6a9c92fb8247c835ec7dc5e97498a4d218e299c5
DanielOgnyanov <escape69@abv.bg>
2025-08-28 18:03:24 +0300

    add news option in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ function Header() {
     <header className="header">
       <div className="logo">📈 Stock Predictor</div>
       <nav className="nav">
+        <a href="#home">News</a>
         <a href="#home">Home</a>
         <a href="#features">Predict</a>
         <a href="#about">Live Prices</a>
