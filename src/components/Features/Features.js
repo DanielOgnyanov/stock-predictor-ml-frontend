946d48bc063a5d2d14053b7a3e3adf77bb64b2d5
DanielOgnyanov <escape69@abv.bg>
2025-08-29 08:52:05 +0300

    set currentSection inside the useEffect

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -20,12 +20,12 @@ const Features = () => {
       { threshold: 0.2 } 
     );
 
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
+    if (currentSection) {
+      observer.observe(currentSection);
     }
 
     return () => {
-      if (sectionRef.current) observer.unobserve(sectionRef.current);
+      if (currentSection) observer.unobserve(currentSection);
     };
   }, []);
 
