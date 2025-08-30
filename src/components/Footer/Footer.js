72486cb3a99b49b8b0f2de275b66889d89e1d116
DanielOgnyanov <escape69@abv.bg>
2025-08-30 09:13:11 +0300

    footer links position change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,19 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
+        
       <div className="footer-container">
-        <p className="footer-text">© {new Date().getFullYear()} Stock Predictor. All rights reserved.</p>
+
         <div className="footer-links">
           <a href="#news">News</a>
           <a href="#predict">Predict</a>
           <a href="#prices">Live Prices</a>
         </div>
+
+        <p className="footer-text">
+          © {new Date().getFullYear()} Stock Predictor. All rights reserved.
+        </p>
+
       </div>
     </footer>
   );
