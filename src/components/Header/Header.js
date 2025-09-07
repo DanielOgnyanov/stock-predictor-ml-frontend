932ae096d99681e7cbf5d89fd5fb2b9d79317b15
DanielOgnyanov <escape69@abv.bg>
2025-09-07 09:43:45 +0300

    add login in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ function Header() {
         <a href="#home">Home</a>
         <a href="#features">Predict</a>
         <a href="#live">Live Prices</a>
+        <a href="#login">Login</a>
        
       </nav>
     </header>
