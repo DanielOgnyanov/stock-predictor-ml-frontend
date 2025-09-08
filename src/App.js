d55c796dc5ba3635f5b6ccf01b34fe6926b0c997
DanielOgnyanov <escape69@abv.bg>
2025-09-08 08:24:25 +0300

    add register component in app.js route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage/IndexPage";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import Header from "../src/components/Header/Header"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
