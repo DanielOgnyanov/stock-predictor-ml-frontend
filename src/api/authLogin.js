d02a222d01f159bc37df114272fdfab9d86fafa6
DanielOgnyanov <escape69@abv.bg>
2025-09-10 08:49:02 +0300

    auth login with axios

---
+++ src/api/authLogin.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/user";
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, {
+      email,
+      password,
+    });
+    return response.data; 
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error; 
+  }
+};
