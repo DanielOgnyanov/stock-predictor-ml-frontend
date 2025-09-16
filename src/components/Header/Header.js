81abd76ba5bd4e3a1dec186efc311bf98caca62d
DanielOgnyanov <escape69@abv.bg>
2025-09-16 19:50:37 +0300

    fisrt setup for the header logged and not logged user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,35 @@
-
-import "./Header.css"; 
+import "./Header.css";
+import { useState } from "react";
 
 function Header() {
+ 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <header className="header">
       <div className="logo">📈 Stock Predictor</div>
       <nav className="nav">
-        
-        <a href="#news">News</a>
-        
-        
-        <a href="#home">Home</a>
-        <a href="#predict">Predict</a>
-        <a href="#live">Live Prices</a>
-        
-        
-         <a href="login">Login</a>
-         <a href="register">Register</a>
-       
+        {isLoggedIn ? (
+          <>
+           
+            <a href="#news">News</a>
+            <a href="#home">Home</a>
+            <a href="#predict">Predict</a>
+            <button 
+              onClick={() => setIsLoggedIn(false)} 
+              className="logout-btn"
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            
+            <a href="#live">Live Prices</a>
+            <a href="login">Login</a>
+            <a href="register">Register</a>
+          </>
+        )}
       </nav>
     </header>
   );
