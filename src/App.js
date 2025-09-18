a38af0c8881f8ab726c65e7c40dc85f40325cb6e
DanielOgnyanov <escape69@abv.bg>
2025-09-18 17:53:10 +0300

    update the app.js to use the auth provider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage/IndexPage";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Header from "../src/components/Header/Header"
+import Header from "./components/Header/Header";
+import { AuthProvider } from "./context/AuthContext"; 
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
