a2f9b4056fdde5675cd178fe1f3371d1711d97da
DanielOgnyanov <escape69@abv.bg>
2025-10-11 15:00:49 +0300

    update the url for the fecth

--- src/api/stockApi.js
+++ src/api/stockApi.js
@@ -1,7 +1,7 @@
 
 export const fetchStocks = async () => {
   try {
-    const response = await fetch("http://localhost:8080/api/stocks");
+    const response = await fetch("http://localhost:8080/api/stocks/latest");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
