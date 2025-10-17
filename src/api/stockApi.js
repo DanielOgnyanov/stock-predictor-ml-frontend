78d931e9e53bd5ee3c0ba5661872bf43777a8e80
DanielOgnyanov <escape69@abv.bg>
2025-10-17 08:47:49 +0300

    create separate call in api folder for the price history by symbol

--- src/api/stockApi.js
+++ src/api/stockApi.js
@@ -12,3 +12,25 @@ export const fetchStocks = async () => {
     return []; 
   }
 };
+
+
+
+
+export const fetchPriceHistory = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/api/stocks/price/history/open");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching price history:", error);
+    throw error;
+  }
+};
+
+
+
