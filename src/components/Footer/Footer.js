268a4a1ef15ff628de2a220cb0d24bf35fb07cb9
DanielOgnyanov <escape69@abv.bg>
2025-10-27 10:14:36 +0200

    adjust the footer link path to match the right one

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
-export default function Footer() {
+const Footer = () => {
   return (
     <footer className="footer">
-        
       <div className="footer-container">
-
         <div className="footer-links">
-          <a href="#news">News</a>
-          <a href="#predict">Predict</a>
-          <a href="#prices">Live Prices</a>
+          <Link to="/livePrices">Live Prices</Link>
+          <Link to="/news">News</Link>
         </div>
 
         <p className="footer-text">
           © {new Date().getFullYear()} Stock Predictor. All rights reserved.
         </p>
-
       </div>
     </footer>
   );
-}
+};
+
+export default Footer;
