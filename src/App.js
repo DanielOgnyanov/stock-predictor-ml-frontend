d69e218e56d15c7f34ee3792a0e2aec96e6221f0
DanielOgnyanov <escape69@abv.bg>
2025-11-01 11:30:09 +0200

    app.js route ok check

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import IndexPage from "./pages/IndexPage/IndexPage";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Header from "./components/Header/Header";
-import { AuthProvider } from "./context/AuthContext"; 
+import { AuthProvider } from "./context/AuthContext";
 import Homepage from "./pages/Homepage/Homepage"
 import Predict from "./pages/Predict/Predict"
 import GlobalBackground from "./components/GlobalBackground/GlobalBackground";
