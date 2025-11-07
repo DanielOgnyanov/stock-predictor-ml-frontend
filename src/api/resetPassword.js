5a3c4732401dacce6ec79e492cfcecb2aeb9f1a6
DanielOgnyanov <escape69@abv.bg>
2025-11-07 10:24:29 +0200

    update the path for the post mapping

--- src/api/resetPassword.js
+++ src/api/resetPassword.js
@@ -1,5 +1,5 @@
 export const resetPassword = async (token, password) => {
-  const response = await fetch("http://localhost:8080/api/auth/update-password", {
+  const response = await fetch("http://localhost:8080/api/auth/reset-password", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ token, newPassword: password }),
--- src/api/sendResetPasswordEmailApi.js
+++ src/api/sendResetPasswordEmailApi.js
@@ -1,5 +1,5 @@
 export const sendResetPasswordEmail = async (email) => {
-  const response = await fetch("http://localhost:8080/api/auth/reset-password", {
+  const response = await fetch("http://localhost:8080/api/auth/request-password-reset", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email }),
