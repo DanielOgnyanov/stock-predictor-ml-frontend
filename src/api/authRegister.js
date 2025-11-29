5633091b1cf89b17e42ac1410304babf3df273a5
DanielOgnyanov <escape69@abv.bg>
2025-11-29 12:14:32 +0200

    adjust the register call and set errors if fail

--- src/api/authRegister.js
+++ src/api/authRegister.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user/register";
+const API_URL =
+  process.env.REACT_APP_API_URL + "/api/user/register" ||
+  "http://localhost:8080/api/user/register";
 
 export const authRegister = async (userData) => {
   try {
@@ -8,10 +10,18 @@ export const authRegister = async (userData) => {
       headers: {
         "Content-Type": "application/json",
       },
+      
+      withCredentials: false, 
     });
+
     return response.data;
   } catch (error) {
-    console.error("Registration failed:", error.response?.data || error.message);
-    throw error.response?.data || error;
+    console.error(
+      "Registration failed:",
+      error.response?.data || error.message
+    );
+
+    
+    throw error.response?.data || { message: "Registration error" };
   }
 };
