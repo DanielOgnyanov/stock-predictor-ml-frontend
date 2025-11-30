9e16e3352485d71bc532390711444cba12cc929a
DanielOgnyanov <escape69@abv.bg>
2025-11-30 11:57:40 +0200

    adjust the api call for aws upload

--- src/api/predictStock.js
+++ src/api/predictStock.js
@@ -1,13 +1,18 @@
+const BASE_URL =
+  process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL + "/api"
+    : "http://localhost:8080/api";
+
 export const fetchLatestPrediction = async (symbol) => {
   try {
-    const response = await fetch(`http://localhost:8080/api/predict/${symbol}/latest`);
+    const response = await fetch(`${BASE_URL}/predict/${symbol}/latest`);
 
     if (!response.ok) {
       throw new Error("Failed to fetch prediction");
     }
 
     const data = await response.json();
-    return data; 
+    return data;
   } catch (err) {
     throw err;
   }
