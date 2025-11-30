380a74559a7bed9a46fdf88f43c2dddae072c124
DanielOgnyanov <escape69@abv.bg>
2025-11-30 11:58:06 +0200

    adjust the api call for aws upload

--- src/api/stockApi.js
+++ src/api/stockApi.js
@@ -1,12 +1,19 @@
 
+const BASE_URL =
+  process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL + "/api"
+    : "http://localhost:8080/api";
+
 export const fetchStocks = async () => {
   try {
-    const response = await fetch("http://localhost:8080/api/stocks/latest");
+    const response = await fetch(`${BASE_URL}/stocks/latest`);
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     const data = await response.json();
-    return data; 
+    return data;
   } catch (error) {
     console.error("Error fetching stocks:", error);
     return []; 
@@ -16,12 +23,20 @@ export const fetchStocks = async () => {
 
 
 
+
+const BASE_URL_ =
+  process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL + "/api"
+    : "http://localhost:8080/api";
+
 export const fetchPriceHistory = async (symbol) => {
   try {
-    const response = await fetch(`http://localhost:8080/api/stocks/price/history/open/${symbol}`);
+    const response = await fetch(`${BASE_URL_}/stocks/price/history/open/${symbol}`);
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     return await response.json();
   } catch (error) {
     console.error("Error fetching price history:", error);
