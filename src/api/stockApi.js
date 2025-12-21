0ef87a21556d7a8d826f8a58e271ad332030c2b6
DanielOgnyanov <escape69@abv.bg>
2025-12-21 17:34:32 +0200

    adjust the api call after added to vercel and render web services

--- src/api/stockApi.js
+++ src/api/stockApi.js
@@ -1,37 +1,38 @@
 
-const BASE_URL =
-  process.env.REACT_APP_API_URL
-    ? process.env.REACT_APP_API_URL + "/api"
-    : "http://localhost:8080/api";
+
+const RAW_BASE_URL = process.env.REACT_APP_API_URL;
+
+
+const BASE_API_URL = RAW_BASE_URL
+  ? `${RAW_BASE_URL}/api`
+  : "http://localhost:8080/api";
+
+if (!BASE_API_URL) {
+  throw new Error("REACT_APP_API_URL is not defined");
+}
+
 
 export const fetchStocks = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/stocks/latest`);
+    const response = await fetch(`${BASE_API_URL}/stocks/latest`);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const data = await response.json();
-    return data;
+    return await response.json();
   } catch (error) {
     console.error("Error fetching stocks:", error);
-    return []; 
+    return [];
   }
 };
 
 
-
-
-
-const BASE_URL_ =
-  process.env.REACT_APP_API_URL
-    ? process.env.REACT_APP_API_URL + "/api"
-    : "http://localhost:8080/api";
-
 export const fetchPriceHistory = async (symbol) => {
   try {
-    const response = await fetch(`${BASE_URL_}/stocks/price/history/open/${symbol}`);
+    const response = await fetch(
+      `${BASE_API_URL}/stocks/price/history/open/${symbol}`
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -43,7 +44,3 @@ export const fetchPriceHistory = async (symbol) => {
     throw error;
   }
 };
-
-
-
-
